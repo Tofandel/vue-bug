0a72024480c36a31ee8edc7bd5700e909aa46001
Tofandel <adrien.foulon@tukan.hu>
2022-07-06 15:58:01 +0200

    first commit

---
+++ src/components/Bug.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <div :class="classes">
+    <p>{{str}}</p>
+    <span><a>Hi bug</a></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: null,
+    str: String
+  }
+}
+</script>
